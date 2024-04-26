af006ffee0c7ead7457e84147099a6343c889e21
EDUARDO SAES MACEDO <eduardo.smacedo2@senacsp.edu.br>
2024-04-26 12:38:52 -0300

    att 26 nada import

--- ex mare.php
+++ ex mare.php
@@ -1,1 +1,101 @@
 <?php
+/* operações aritiméticos
+ + soma,
+ - subtração,
+ * multiplicção,
+ / divisão
+ */
+ 
+/* operadores relacionais
+> maior,
+< menor,
+== iglual,
+>= maoir e igual
+<= maior e ugual
+!= diferente
+*/
+ 
+/* operadores logicos
+and (e)
+or (ou)
+ot (negação)
+ */
+
+/* legal */
+/*
+NAO PODE TER ESPAÇO
+NAO PODE TER CARACTERES ESPECIAIS
+NAO PODE TER ACENTO
+NAO PODE TER COMEÇAR COM NUMERO
+PRECISA COMEÇAR COM $
+PRECISA FAZER REFERENCIA AO SEU NUMERO  
+*/
+//EXEMPLO 1
+$nomevar;
+//EXEMPLO 2
+$nomevar = 'hello word';
+//EXEMPLO 3
+$n1 = 10;
+$verdad = true;
+$total = -15.5;
+$nomevar;
+
+
+//noice
+echo"hello word <br> <br>";
+echo "foda <br> <br>";
+nl2br()echo' <br> <br>
+
+
+wWWWw               wWWWw
+vVVVv (___) wWWWw         (___)  vVVVv
+(___)  ~Y~  (___)  vVVVv   ~Y~   (___)
+ ~Y~   \|    ~Y~   (___)    |/    ~Y~
+ \|   \ |/   \| /  \~Y~/   \|    \ |/
+\\|// \\|// \\|/// \\|//  \\|// \\\|///
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+              Art By jgs
+
+<br> <br>
+';
+
+
+
+
+$n1 = 2;
+/* Estruturas de comparação
+compara dois ou mais valores e devolve uma
+resposta verdadira ou falsa dependendo da
+análise das condições apresentadas.
+/* operações aritmétricas
++ soma
+- subtração
+* multiplicação
+/ divisão
+*/
+ 
+/* operadores relacionais
+> Maior
+< Menor
+== Igual
+>= Maior e igual
+<= Menor e igual
+!= diferente
+*/
+ 
+/* operadores lógicos
+and (E)
+Or (Or)
+Not (Negação)
+*/
+ 
+ // comparação simples
+if ($n1 + 10 >= 12 ) {
+    echo 'n1 é maior que 2!';
+}
+if ($user == 'Joaquim' AND $senha == 12345) {
+    # code...
+}
+ 
+
+?>
